d9c6137dcae67084ffa101ab0a11b6174a956c03
bndckymt <benedickyamat2@gmail.com>
2022-12-02 14:10:05 +0800

    merge

--- load processing system/main.cpp
+++ load processing system/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+cout<<"/n";
+cout<<"--------------------------------------------";
+cout<<"|                                          |";
+cout<<"|                                          |";
+cout<<"--------------------------------------------";
 }

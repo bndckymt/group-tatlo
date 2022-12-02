d1419bcc19eccf0f3b4e94961caa3038f8825b70
Ran1302 <randyjr.y.antalan@isu.edu.ph>
2022-12-02 14:09:00 +0800

    MAIN MENU

--- load processing system/main.cpp
+++ load processing system/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
 
+void displaymenu()
+{
+    int num1;
+    cout<<"===================================================== \n";
+    cout<<" \t\tSELECT OPTION \t \n ";
+    cout<<"===================================================== \n";
+    cout<<" 1.Direct Load\n";
+    cout<<" 2.Regular Load\n";
+    cout<<" 3.Transaction\n";
+    cout<<" 4.Exit\n";
+    cout << "Enter Your Choice: ";
+    cin >> num1;
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    displaymenu();
 }
